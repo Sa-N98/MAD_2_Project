ad898e2dc70f4edf35dbf667245aa631586ef28a
Saranya Nayak <135203195+Sa-N98@users.noreply.github.com>
2023-06-11 11:50:39 +0530

    changes made

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
 import os
-import requests
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Resource, Api, reqparse, abort
-from sqlalchemy import and_
-
-current_dir = os.path.abspath(os.path.dirname(__file__))
+from flask import Flask , render_template
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + \
-    os.path.join(current_dir, "database.sqlite3")
-api = Api(app)
 
-# create the extension
-db = SQLAlchemy()
-# initialize the app with the extension
-db.init_app(app)
 app.app_context().push()
 
 
+@app.route("/")
+def hello_world():
+    var='this is flask'
+    return(render_template('landingPage.html', Var=var))
 
 
 
