743161ee63756fa2b89347117f91b90a461642d6
Saranya Nayak <135203195+Sa-N98@users.noreply.github.com>
2023-06-22 00:29:17 +0530

    got the login form to work sort of!
    The login form works but with the route userlogin and not login.
    
    So have to fix the template for loginrequired

---
+++ application/model.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_security import  UserMixin, RoleMixin
+db = SQLAlchemy()
+
+roles_users= db.Table('roles_users',
+                      db.Column('user_id', db.Integer, db.ForeignKey('User.id')),
+                      db.Column('role_id', db.Integer, db.ForeignKey('Role.id'))
+                      )
+class User(db.Model,UserMixin):
+    __tablename__ = 'User'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String,unique=True, nullable=False)
+    password=db.Column(db.String)
+    active=db.Column(db.Boolean)
+    fs_uniquifier = db.Column(db.String)
+    roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users',lazy='dynamic'))
+
+class Role(db.Model,RoleMixin):
+    __tablename__ = 'Role'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    description= db.Column(db.String,nullable=False)
--- main.py
+++ main.py
@@ -1,74 +1,60 @@
 import os
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask , render_template ,request, redirect, url_for
-from flask_security import Security, SQLAlchemyUserDatastore, login_required, UserMixin , RoleMixin
+from application.model import *
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField
+from flask import Flask, render_template, request, redirect, url_for
+from flask_security import Security, SQLAlchemyUserDatastore, login_required, logout_user, login_user
 from flask_security.utils import hash_password
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SECRET_KEY']='superseccret'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(current_dir, "database.sqlite3")
+app.config['SECRET_KEY']='superseccret'
 app.config['SECURITY_PASSWORD_SALT'] = 'salt'
 
 
-db = SQLAlchemy()
 db.init_app(app)
-
 app.app_context().push()
 
-
-
-
-class users(db.Model, UserMixin):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, autoincrement=True,  primary_key=True, nullable=False)
-    username = db.Column(db.String, unique=True)
-    email= db.Column(db.String,unique=True)
-    password = db.Column(db.String)
-    active = db.Column(db.Boolean)
-    fs_uniquifier = db.Column(db.String)
-    roles = db.relationship('role', secondary='roles_users', backref=db.backref('users', lazy= 'dynamic'))
-
-class roles_users(db.Model):
-      __tablename__ ='roles_users'
-      user_id= db.Column(db.Integer, db.ForeignKey ('users.id'),primary_key=True, nullable=False)
-      role_id=db.Column(db.Integer, db.ForeignKey ('role.id'), primary_key=True, nullable=False)
-
-
-class role(db.Model,  RoleMixin):
-    __tablename__ = 'role'
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(255))
    
-user_datastore=SQLAlchemyUserDatastore(db,users,None)
-security=Security(app, user_datastore)
 
-@app.route("/", methods=["GET", "POST"])
-def hello_world():
-    if request.method=="POST":
-        user_datastore.create_user(
-            username=request.form.get('username'),
-            email=request.form.get('email'),
-            password=hash_password(request.form.get('password')),
-        )
-        passwords=hash_password(request.form.get('password'))
-        repasswords=hash_password(request.form.get('re-password'))
-        print(passwords)
-        print(repasswords)
+user_datastore=SQLAlchemyUserDatastore(db, User, Role)
+security= Security(app, user_datastore)
+
+@app.route("/", methods=['POST','GET'])
+def index():
+    if request.method =="POST":
+        user_datastore.create_user(username=request.form.get('username'),
+                                   email=request.form.get('email'),
+                                   password= hash_password(request.form.get('password')))
         db.session.commit()
-        return redirect (url_for('profile'))
-    sas=users.query.all()
-    return(render_template('landingPage.html', user=sas))
+        return redirect(url_for('login'))
+    hat="hat"
+    return render_template('index.html',var=hat)
 
+@app.route('/userlogin', methods=['GET', 'POST'])
+def login():
+    if request.method =='POST':
+        print(request.form.get('email'))
+        user = user_datastore.find_user(email=request.form.get('email'))
+        login_user(user)
+        return redirect(url_for('profile'))
+    return render_template('login_user.html')
 
 @app.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.hmtl')
+    return render_template('profile.html')
+
+
+# use this rout in link in template to log out 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.debug = True
     app.run()
-
