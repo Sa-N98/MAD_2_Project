8a7c1fc4f75da0231664d00c4e61d4c2a495c3f1
Saranya Nayak <135203195+Sa-N98@users.noreply.github.com>
2023-06-25 00:56:19 +0530

    Login and Signup Page done

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import os
 from application.model import *
-from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired
-from wtforms import StringField, PasswordField, SubmitField
 from flask import Flask, render_template, request, redirect, url_for
 from flask_security import Security, SQLAlchemyUserDatastore, login_required, logout_user, login_user
 from flask_security.utils import hash_password, verify_password
@@ -26,18 +23,17 @@ security= Security(app, user_datastore)
 
 
 
-
-
+'''
+lodes the login and sign up page.
+this route is also used for login validation.
+'''
 @app.route("/", methods=['POST','GET'])
 def index():
-
-
     if request.method =="POST":
          form_type = request.form['form_type']
          username = request.form['username']
          email = request.form['email']
          password = request.form['password']
-
 
          if form_type == 'login':
             user = User.query.filter_by(email=request.form.get('email')).first()
@@ -47,9 +43,14 @@ def index():
             else:
                 massage="User Not Found. Please Signup"
                 return render_template('login_and_signup.html', massage=massage)
+   
     return render_template('login_and_signup.html')
 
 
+'''
+This route is used for submition od the sign up form.
+the form is submitted using js.
+'''
 @app.route('/submit',methods=['POST'])
 def submit():
     user_datastore.create_user(username=request.form.get('username'),
@@ -83,14 +84,6 @@ def click():
 
 
 
-@app.route('/userlogin', methods=['GET', 'POST'])
-def login():
-    if request.method =='POST':
-        print(request.form.get('email'))
-        user = user_datastore.find_user(email=request.form.get('email'))
-        login_user(user)
-        return redirect(url_for('profile'))
-    return render_template('login_user.html')
 
 @app.route('/profile')
 @login_required
