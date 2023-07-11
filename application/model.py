c7b78ab87d42d593977446334198f2ea002db28b
Saranya Nayak <135203195+Sa-N98@users.noreply.github.com>
2023-07-11 22:50:20 +0530

    updated the landing page and made the filter page

--- application/model.py
+++ application/model.py
@@ -34,6 +34,47 @@ class show(db.Model):
     rating = db.Column(db.Integer, nullable=False)
     poster = db.Column(db.String, unique=False, nullable=False)
     posterLong = db.Column(db.String, unique=False, nullable=False)
-    # venue = db.relationship("venue", secondary="Show_Venue")
-    # dates = db.relationship("date", secondary="Show_Venue")
-    # genre = db.relationship("genre", secondary="movie_g")
+    venue = db.relationship("venue", secondary="Show_Venue")
+    dates = db.relationship("date", secondary="Show_Venue")
+    genre = db.relationship("genre", secondary="movie_g")
+
+class venue(db.Model):
+    __tablename__ = 'venue'
+    id = db.Column(db.Integer, autoincrement=True,
+                   primary_key=True, unique=True)
+    name = db.Column(db.String, unique=False, nullable=False)
+    place = db.Column(db.String, unique=False, nullable=False)
+
+
+class Show_Venue(db.Model):
+    __tablename__ = 'Show_Venue'
+    s_id = db.Column(db.Integer, db.ForeignKey("show.id"),
+                     primary_key=True, nullable=False)
+    v_id = db.Column(db.Integer, db.ForeignKey("venue.id"),
+                     primary_key=True, nullable=False)
+    d_id = db.Column(db.Integer, db.ForeignKey("date.id"),
+                     primary_key=True, nullable=False)
+    seats = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+
+
+class movie_g(db.Model):
+    __tablename__ = 'movie_g'
+    m_id = db.Column(db.Integer, db.ForeignKey("show.id"),
+                     primary_key=True, nullable=False)
+    g_id = db.Column(db.Integer, db.ForeignKey("genre.id"),
+                     primary_key=True, nullable=False)
+
+
+class date(db.Model):
+    __tablename__ = 'date'
+    id = db.Column(db.Integer, autoincrement=True,
+                   primary_key=True, unique=True)
+    dates = db.Column(db.String, unique=True, nullable=False)
+
+
+class genre(db.Model):
+    __tablename__ = 'genre'
+    id = db.Column(db.Integer, autoincrement=True,
+                   primary_key=True, unique=True)
+    type = db.Column(db.String, unique=True, nullable=False)
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
+from sqlalchemy import and_
 from application.model import *
-from flask import Flask, render_template, request, redirect, url_for
-from flask_security import Security, SQLAlchemyUserDatastore, login_required, logout_user, login_user
 from flask_security.utils import hash_password, verify_password
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_security import Security, SQLAlchemyUserDatastore, login_required, logout_user, login_user
+
+
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 
@@ -10,6 +13,9 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(current_dir, "database.sqlite3")
 app.config['SECRET_KEY']='superseccret'
 app.config['SECURITY_PASSWORD_SALT'] = 'salt'
+app.jinja_options = app.jinja_options.copy()
+app.jinja_options['variable_start_string'] = '[[ '
+app.jinja_options['variable_end_string'] = ' ]]'
 
 
 db.init_app(app)
@@ -33,7 +39,7 @@ def index():
             user = User.query.filter_by(email=request.form.get('email')).first()
             if user and verify_password(password, user.password):
                 login_user(user)
-                return redirect(url_for('profile'))
+                return redirect(url_for('welcome'))
             else:
                 massage="User Not Found. Please Signup"
                 return render_template('login_and_signup.html', massage=massage)
@@ -53,15 +59,79 @@ def submit():
     db.session.commit()
     return "submition"
 
+
 @app.route('/click')
 def click():
     return "true"
 
+@app.route('/api/movies')
+def movies():
+    last_five_rows = show.query.order_by(show.id.desc()).limit(5).all()
+    last_five_movies_data = list()
+    for data in  last_five_rows :
+        last_five_movies_data.append(
+            {
+            'name':data.name,
+            'posterLong':data.posterLong,
+            'poster':data.poster,
+            'rating':data.rating,
+             }
+        )
+    return jsonify(last_five_movies_data)
 
-@app.route('/profile')
+@app.route('/api/movie/<title>')
+def movie(title):
+    Show = show.query.filter(show.name == title).all()
+    movie_data= {
+            'name': Show[0].name,
+            'poster':Show[0].poster,
+            'rating':Show[0].rating,
+            }
+    print(movie_data) 
+    return jsonify(movie_data)
+
+@app.route('/welcome')
 @login_required
-def profile():
-    return render_template('profile.html')
+def welcome():
+    movie = show.query.all()
+    venues = venue.query.all()
+    genres = genre.query.all()
+    return render_template('welcome.html' , movies=movie , venue=venues, genre=genres)
+
+@app.route('/filter', methods=["GET", "POST"])
+@login_required
+def filter():
+    if request.method == "POST":
+        m_genre = request.form['genre']
+        m_venue = request.form['place']
+        m_rating = request.form['rating']
+        if m_genre != 'none' and m_venue != 'none' and m_rating != 'none':
+            movie = show.query.filter(and_(show.genre.any(type=m_genre), show.venue.any(
+                name=m_venue), show.rating.like(f'{m_rating}%'))).all()
+            return render_template('filter.html', g=m_genre, v=m_venue, r=m_rating, movies=movie)
+        elif m_genre != 'none' and m_venue != 'none' and m_rating == 'none':
+            movie = show.query.filter(
+                and_(show.genre.any(type=m_genre), show.venue.any(name=m_venue))).all()
+            return render_template('filter.html', g=m_genre, v=m_venue, r=m_rating, movies=movie)
+        elif m_genre != 'none' and m_venue == 'none' and m_rating == 'none':
+            movie = show.query.filter(and_(show.genre.any(type=m_genre))).all()
+            return render_template('filter.html', g=m_genre, v=m_venue, r=m_rating, movies=movie)
+        elif m_genre == 'none' and m_venue != 'none' and m_rating == 'none':
+            movie = show.query.filter(and_(show.venue.any(name=m_venue))).all()
+            return render_template('filter.html', g=m_genre, v=m_venue, r=m_rating, movies=movie)
+        elif m_genre == 'none' and m_venue == 'none' and m_rating != 'none':
+            movie = show.query.filter(
+                and_(show.rating.like(f'{m_rating}%'))).all()
+            return render_template('filter.html', g=m_genre, v=m_venue, r=m_rating, movies=movie)
+        elif m_genre != 'none' and m_venue == 'none' and m_rating != 'none':
+            movie = show.query.filter(
+                and_(show.genre.any(type=m_genre), show.rating.like(f'{m_rating}%'))).all()
+            return render_template('filter.html', g=m_genre, v=m_venue, r=m_rating, movies=movie)
+        elif m_genre == 'none' and m_venue != 'none' and m_rating != 'none':
+            movie = show.query.filter(
+                and_(show.venue.any(name=m_venue), show.rating.like(f'{m_rating}%'))).all()
+            return render_template('filter.html', g=m_genre, v=m_venue, r=m_rating, movies=movie)
+  
 
 
 # use this rout in link in template to log out 
